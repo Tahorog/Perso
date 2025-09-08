b601036addd7dcc7bcd20736feda2c76eea31e9f
Tahorog <146565416+Tahorog@users.noreply.github.com>
2025-09-08 17:11:36 +0200

    Create Exo4.java

---
+++ Java/Chap2/Exo4.java
@@ -0,0 +1,6 @@
+public class Exo4 {
+    //Une personne peut être propriétaire d’une ou plusieurs maisons.
+    //une maison a un ou plusieurs propriétaires.
+    //Une personne peut être locataire d’une ou plusieurs maisons.
+    //une maison peut être louée par une ou plusieurs personnes.
+}
